b5c54d52cb007fd6cd036da4f15a74d6c8e34127
xwidghet <xwidghet@gmail.com>
2026-01-15 05:33:04 -0800

    Minor changes to Memory Mapper to increase performance. Seems I may need to make a good optimization for address remapping as my solution drops speed by 10-20%. It's still 2.8X realtime on my machine, but that seems slow for a NES emulator.

--- src/MemoryMapper.cpp
+++ src/MemoryMapper.cpp
@@ -31,30 +31,31 @@ void MemoryMapper::Init(PPU* PPU)
 
 uint32_t MemoryMapper::MapAddress(uint32_t Address)
 {
+    // Prg ROM Area, with iNES 2.0 ROMS it could be as small as 8KB X 4 Mirrors, but generally 16KB X 2 Mirrors or 32KB No Mirrors
+    if (Address >= 0x8000)
+        Address = 0x8000 + (Address - 0x8000) % (mPrgRomMemory.size());
     // Internal RAM, 0x0000 - 0x07FF, Mirrored up to 0x1FFF
-    if (Address <= 0x1FFF)
+    else if (Address <= 0x1FFF)
         Address = 0x0000 + (Address - 0x0000) % (0x07FF + 1);
     // NES PPU Registers, 0x2000 - 0x2007, Mirrored up to 0x3FFF
     else if (Address <= 0x3FFF)
         Address = 0x2000 + (Address - 0x2000) % ((0x2007 - 0x2000) + 1);
-    // Prg ROM Area, with iNES 2.0 ROMS it could be as small as 8KB X 4 Mirrors, but generally 16KB X 2 Mirrors or 32KB No Mirrors
-    else if (Address >= 0x8000)
-        Address = 0x8000 + (Address - 0x8000) % (mPrgRomMemory.size());
 
     return Address;
 }
 
 uint8_t MemoryMapper::Read8Bit(const uint32_t Address)
 {
-    uint8_t Value = mMemory[MapAddress(Address)];
+    uint32_t TargetAddress = MapAddress(Address);
+    uint8_t Value = mMemory[TargetAddress];
 
     if (Address == STATUS_ADDRESS)
     {
-        mMemory[MapAddress(Address)] = Value & (~static_cast<uint8_t>(ESTATUS_READ_MASKS::FRAME_INTERRUPT));
+        mMemory[TargetAddress] = Value & (~static_cast<uint8_t>(ESTATUS_READ_MASKS::FRAME_INTERRUPT));
     }
     else if (Address == PPUSTATUS_ADDRESS)
     {
-        mMemory[MapAddress(Address)] = Value & (~static_cast<uint8_t>(EPPUSTATUS::VBLANK_FLAG));
+        mMemory[TargetAddress] = Value & (~static_cast<uint8_t>(EPPUSTATUS::VBLANK_FLAG));
         mPPU->ClearWRegister();
     }
 
@@ -69,7 +70,6 @@ void MemoryMapper::Write8Bit(const uint32_t Address, uint8_t Value)
 
     if (Address == PPUADDR_ADDRESS || Address == PPUSCROLL_ADDRESS)
     {
-        mMemory[MapAddress(Address)] = Value & (~static_cast<uint8_t>(EPPUSTATUS::VBLANK_FLAG));
         mPPU->ToggleWRegister();
     }
 
